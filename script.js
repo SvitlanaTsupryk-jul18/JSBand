13c30abd99a4994c4ddfe749fd65982023206fbf
Svet <stsupryk@gmail.com>
2019-11-02 11:00:37 +0200

    implement adding and editing functions

--- script.js
+++ script.js
@@ -6,45 +6,122 @@
     const modal = document.querySelector(".modal");
     const buttons = document.querySelector(".modal__buttons");
     const root = document.querySelector(".root");
-    const todoNew = document.querySelector(".new__title");
+    const titleNew = document.querySelector(".new__title");
+    const descriptionNew = document.querySelector(".new__description");
     const select = document.querySelector(".new__priority");
-    const template = document.querySelector("#template");
-
     let todoList = [];
+    let flagEdit = false;
+    let currentId;
+
+    const createTodo = item => {
+      const todo = document.createElement("div");
+      todo.classList.add("todo");
+      const title = document.createElement("h3");
+      title.classList.add("todo__title");
+      const description = document.createElement("p");
+      description.classList.add(".todo__description");
+      const priority = document.createElement("div");
+      priority.classList.add(".todo__priority");
+      const btn = document.createElement("button");
+      btn.classList.add(".todo__change");
+      btn.innerHTML = "...";
+      const buttonsWrapper = document.createElement("div");
+      buttons.classList.add(".todo__buttons");
+      const editBtn = document.createElement("button");
+      editBtn.classList.add(".edit");
+      editBtn.value = "edit";
+      editBtn.innerHTML = "edit";
+      const doneBtn = document.createElement("button");
+      doneBtn.classList.add(".open");
+      doneBtn.value = "done";
+      doneBtn.innerHTML = "done";
+      const deleteBtn = document.createElement("button");
+      deleteBtn.classList.add(".delete");
+      deleteBtn.value = "delete";
+      deleteBtn.innerHTML = "delete";
+      buttonsWrapper.append(editBtn);
+      buttonsWrapper.append(doneBtn);
+      buttonsWrapper.append(deleteBtn);
+      todo.append(title);
+      todo.append(description);
+      todo.append(priority);
+      todo.append(btn);
+      todo.append(buttonsWrapper);
+      root.append(todo);
+
+      if (item.open === false) {
+        todo.classList.add("done");
+      }
+
+      title.innerHTML = item.title;
+      priority.innerHTML = item.priority;
+      todo.dataset.id = item.id;
+      item.description
+        ? (description.innerHTML = item.description)
+        : (description.innerHTML = "");
+      titleNew.value = "";
+      descriptionNew.value = "";
+
+      doneBtn.addEventListener("click", e => {
+        currentId = +e.target.parentElement.parentElement.dataset.id;
+        e.target.parentElement.parentElement.classList.toggle("done");
+
+        todoList.map(todo => {
+          if (todo.id === currentId) {
+            todo.open = !todo.open;
+          }
+        });
+
+        sessionStorage.setItem("todos", JSON.stringify(todoList));
+      });
+
+      editBtn.addEventListener("click", e => {
+        modal.classList.add("show");
+        currentId = +e.target.parentElement.parentElement.dataset.id;
+        let item = todoList.find(todo => todo.id === currentId);
+        titleNew.value = item.title;
+        item.description
+          ? (descriptionNew.value = item.description)
+          : (descriptionNew.value = "");
+        select.value = item.priority;
+        flagEdit = true;
+      });
+    };
 
     const addTodo = () => {
-      if (!todoNew.value) return;
+      if (!titleNew.value) return;
       let temp = {};
-      temp.title = todoNew.value;
-      temp.check = false;
+      temp.title = titleNew.value;
+      temp.description = descriptionNew.value;
+      temp.open = true;
       temp.priority = select.value;
-      console.log(todoList);
-      let n = todoList.length;
-      todoList[n] = temp;
-      createTodo([todoList[n]]);
+      temp.id = Date.now();
+      todoList.push(temp);
+      createTodo(temp);
       sessionStorage.setItem("todos", JSON.stringify(todoList));
     };
 
-    const createTodo = list => {
-      console.log(list);
-      for (let key in list) {
-        const todo = document.importNode(template.content, true);
-        const title = todo.querySelector(".todo__title");
-        const description = todo.querySelector(".todo__description");
-        const priority = todo.querySelector(".todo__priority");
-
-        root.appendChild(todo);
-        console.log(key);
-        title.innerHTML = list[key].title;
-        priority.innerHTML = list[key].priority;
-      }
-
-      todoNew.value = "";
+    const editTodo = id => {
+      todoList.map(todo => {
+        if (todo.id === currentId) {
+          todo.title = titleNew.value;
+          todo.description = descriptionNew.value;
+          todo.priority = select.value;
+        }
+      });
+      console.log(todoList);
+      sessionStorage.setItem("todos", JSON.stringify(todoList));
+      root.innerHTML = "";
+      todoList.forEach(item => {
+        createTodo(item);
+      });
     };
 
     if (sessionStorage.getItem("todos") !== null) {
       todoList = JSON.parse(sessionStorage.getItem("todos"));
-      createTodo(todoList);
+      todoList.forEach(item => {
+        createTodo(item);
+      });
     }
 
     const showModal = () => {
@@ -54,8 +131,13 @@
     const closeModal = e => {
       modal.classList.remove("show");
       if (e.target.name === "save") {
-        addTodo();
+        if (flagEdit) {
+          editTodo(currentId);
+        } else {
+          addTodo();
+        }
       }
+      flagEdit = false;
     };
 
     modalbtn.addEventListener("click", showModal);
